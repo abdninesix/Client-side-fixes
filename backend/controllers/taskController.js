888b589564794ae37e0b3e6b73db8253b2ee13cc
alishasaif13 <alisha.saif456@gmail.com>
2025-11-21 21:19:06 -0800

    all

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -31,7 +31,21 @@ export const readSingleTask = async (req, res) => {
 };
 
 // update task
-export const updateTask = async () => {};
+export const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ message: "success in updating task" });
+  } catch (error) {
+    res.status(500).json({ message: "Error in updating task" });
+  }
+};
 
 // delete task
-export const deleteTask = async () => {};
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "delete success task" });
+  } catch (error) {
+    res.status(500).json({ message: "Error in deleting task" });
+  }
+};
--- backend/routes/taskRouter.js
+++ backend/routes/taskRouter.js
@@ -1,8 +1,21 @@
-import express from 'express';
-import { createTask, readAllTask, readSingleTask } from '../controllers/taskController.js';
-const router=express.Router()
+import express from "express";
+import {
+  createTask,
+  deleteTask,
+  readAllTask,
+  readSingleTask,
+  updateTask,
+} from "../controllers/taskController.js";
 
-router.post('/', createTask )
-router.get('/', readAllTask )
-router.get('/:id', readSingleTask )
+const router = express.Router();
+//dynamic routes
+router.get("/:id", readSingleTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+// static routes
+router.post("/", createTask);
+router.get("/", readAllTask);
+
+
 export default router;
