a6a8cd59dfc752ea1e17a6d995901035a0fd887f
abdninesix <mohabd1995@gmail.com>
2025-11-23 23:54:04 +0500

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,33 +64,35 @@ function App() {
     <div className="min-h-screen flex items-center justify-center flex-col px-8" style={{ backgroundImage: `url('/back.jpeg')`, backgroundSize: "cover" }}>
 
       {/* Name */}
-      <span className="absolute top-4 left-4 text-lg font-semibold uppercase p-2 text-white bg-green-300/60 rounded-full">Todo by Alisha Saif</span>
+      <span className="absolute top-10 text-xl font-bold uppercase p-2 text-white bg-red-300/60 rounded-xl">Todo by Ali Shah</span>
 
       <div className="bg-white/60 shadow-lg w-full md:w-4/5 p-6 space-y-6 rounded-xl text-gray-600 duration-200">
         {/* Input */}
         <h1 className="text-4xl font-semibold">Add a new task</h1>
-
         <form
           onSubmit={(e) => {
             e.preventDefault();
             editId ? updateTask() : addTask();
           }}
-          className="flex gap-2"
+          className="flex gap-6"
         >
-          <div className="w-full flex flex-col gap-2">
+          <div className="w-full flex flex-col gap-6">
             <input
               type="text"
               placeholder="Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="bg-white rounded-md p-2 w-full"
+              className="bg-white rounded-md p-2 outline-pink-300 w-full"
+              required
             />
-            <input
+            <textarea
               type="text"
               placeholder="Description"
+              rows={2}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="bg-white rounded-md p-2 w-full"
+              className="bg-white rounded-md p-2 outline-pink-300 w-full"
+              required
             />
           </div>
           <button className="bg-white rounded-md p-2 text-4xl cursor-pointer">{editId ? "✔️" : "+"}</button>
@@ -102,23 +104,20 @@ function App() {
         {todos.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {todos.map((todo) => (
-              <div
-                key={todo._id}
-                className="flex items-center justify-between bg-white rounded-md p-6"
-              >
+              <div key={todo._id} className="flex items-center justify-between bg-white rounded-md p-6">
                 <div>
                   <h2 className="text-xl font-semibold">{todo.name}</h2>
-                  <p className="text-base">{todo.description}</p>
-                  <p className="text-xs">Created {format(todo.createdAt)}</p>
+                  <p className="text-base">{todo.description.length < 25 ? (todo.description) : (todo.description.slice(0, 25) + "...")}</p>
+                  <p className="text-xs mt-2">Created {format(todo.createdAt)}</p>
                 </div>
-                <div className="flex items-center gap-2 text-xl">
+                <div className="flex flex-col items-center gap-2 text-xl">
                   <button onClick={() => startEdit(todo)} className="cursor-pointer hover:scale-110">✏️</button>
                   <button onClick={() => deleteTask(todo._id)} className="cursor-pointer hover:scale-110">🗑️</button>
                 </div>
               </div>
             ))}
           </div>
-        ) : (<p>List is Empty</p>)}
+        ) : (<p className="font-semibold">List is Empty</p>)}
       </div>
     </div>
   );
