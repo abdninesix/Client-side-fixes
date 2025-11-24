f9c7cdaff565b62322eb9b9b5287032b32f376b4
abdninesix <mohabd1995@gmail.com>
2025-11-24 15:27:38 +0500

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 
 function App() {
 
